4058118bfcb8a4a7be8193c87c6b983c2840d7d0
Diego Silva <diegosilva157890@gmail.com>
2023-10-19 18:39:02 -0300

    Base projeto protocolo

---
+++ protocolo/src/components/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="App">
+      <text>dededeededdeedde</text>
+    </div>
+  );
+}
+
+
---
+++ protocolo/src/components/TabelaSeparacao/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default function TabelaSeparacao() {
+  return (
+    <div className="App">
+      <text>dededeededdeedde</text>
+    </div>
+  );
+}
+
+
