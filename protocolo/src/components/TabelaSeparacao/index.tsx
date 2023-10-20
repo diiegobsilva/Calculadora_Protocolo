3c93660d28e9133f3481a65dd2df6041863ede9e
Diego Silva <diegosilva157890@gmail.com>
2023-10-20 09:41:48 -0300

    Primeira parte da função Separete

--- protocolo/src/components/TabelaSeparacao/index.tsx
+++ protocolo/src/components/TabelaSeparacao/index.tsx
@@ -1,18 +1,84 @@
 import { Table } from "react-bootstrap";
 import './index.css';
+import { useState } from "react";
+
+interface SeparationResult {
+  protocolo: string;
+  startBit: string;
+  tamanhoProtocolo: string;
+  numeroProtocolo: string;
+  informacaoContida: string;
+  informacaoSequencia: string;
+  errorCheck: string;
+  stopBit: string;
+}
 
 export default function TabelaSeparacao() {
+  const [valorProtocolo, setValorProtocolo] = useState("");
+  const [tableData, setTableData] = useState<SeparationResult | null>(null);
+
+  const handleSeparacaoClick = () => {
+    if (valorProtocolo) {
+      const prefixo = valorProtocolo.substring(0, 4);
+      let protocolo, startBit, tamanhoProtocolo, numeroProtocolo, informacaoContida, informacaoSequencia, errorCheck, stopBit;
+
+      if (prefixo === "7878") {
+        protocolo = valorProtocolo.substring(0, 4);
+        startBit = valorProtocolo.substring(4, 6);
+        tamanhoProtocolo = valorProtocolo.substring(6, 8);
+        numeroProtocolo = valorProtocolo.substring(8, valorProtocolo.length - 16);
+        informacaoContida = valorProtocolo.substring(valorProtocolo.length - 16, valorProtocolo.length - 12);
+        informacaoSequencia = valorProtocolo.substring(valorProtocolo.length - 12, valorProtocolo.length - 8);
+        errorCheck = valorProtocolo.substring(valorProtocolo.length - 8, valorProtocolo.length - 4);
+        stopBit = valorProtocolo.substring(valorProtocolo.length - 4);
+      } else if (prefixo === "7979") {
+        protocolo = valorProtocolo.substring(0, 4);
+        startBit = valorProtocolo.substring(4, 8);
+        tamanhoProtocolo = valorProtocolo.substring(8, 10);
+        numeroProtocolo = valorProtocolo.substring(10, valorProtocolo.length - 16);
+        informacaoContida = valorProtocolo.substring(valorProtocolo.length - 16, valorProtocolo.length - 12);
+        informacaoSequencia = valorProtocolo.substring(valorProtocolo.length - 12, valorProtocolo.length - 8);
+        errorCheck = valorProtocolo.substring(valorProtocolo.length - 8, valorProtocolo.length - 4);
+        stopBit = valorProtocolo.substring(valorProtocolo.length - 4);
+      } else {
+        console.log("Prefixo incorreto");
+        setTableData({
+          protocolo: "",
+          startBit: "",
+          tamanhoProtocolo: "",
+          numeroProtocolo: "",
+          informacaoContida: "",
+          informacaoSequencia: "",
+          errorCheck: "",
+          stopBit: "",
+        });
+        return;
+      }
+
+      setTableData({ protocolo, startBit, tamanhoProtocolo, numeroProtocolo, informacaoContida, informacaoSequencia, errorCheck, stopBit });
+    }
+  };
+
+  const handleLimpezaClick = () => {
+    setValorProtocolo("");
+    setTableData(null);
+  };
+
   return (
     <div className="app-container">
       <div className="input-container">
         <div className="labelProtocolo">
-           <label className="label">Protocolo</label>
-           <input type="text"  className="input"/>
+          <label className="label">Protocolo</label>
+          <input type="text" className="input" onChange={(e) => setValorProtocolo(e.target.value)} value={valorProtocolo} />
         </div>
-  
+
         <div className="containerBotao">
-          <button className="btn btn-separacao">SEPARAÇÃO</button>
-          <button className="btn btn-limpeza">LIMPEZA</button>
+          <button className="btn btn-separacao" onClick={handleSeparacaoClick}>
+            SEPARAÇÃO
+          </button>
+          <button className="btn btn-limpeza" onClick={handleLimpezaClick}>
+            LIMPEZA
+          </button>
         </div>
       </div>
 
@@ -32,14 +98,14 @@ export default function TabelaSeparacao() {
           </thead>
           <tbody>
             <tr>
-              <td>teste</td>
-              <td>teste</td>
-              <td>teste</td>
-              <td>teste</td>
-              <td>teste</td>
-              <td>teste</td>
-              <td>teste</td>
-              <td>teste</td>
+              <td>{tableData?.protocolo}</td>
+              <td>{tableData?.startBit}</td>
+              <td>{tableData?.tamanhoProtocolo}</td>
+              <td>{tableData?.numeroProtocolo}</td>
+              <td>{tableData?.informacaoContida}</td>
+              <td>{tableData?.informacaoSequencia}</td>
+              <td>{tableData?.errorCheck}</td>
+              <td>{tableData?.stopBit}</td>
             </tr>
           </tbody>
         </Table>
