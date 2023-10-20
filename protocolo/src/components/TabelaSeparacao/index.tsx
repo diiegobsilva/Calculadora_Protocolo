ef275380bc5bea52fbc7dc8da624675e47d0c5cf
Diego Silva <diegosilva157890@gmail.com>
2023-10-20 00:10:52 -0300

    Tabela Separação

--- protocolo/src/components/TabelaSeparacao/index.tsx
+++ protocolo/src/components/TabelaSeparacao/index.tsx
@@ -1,12 +1,49 @@
-import React from 'react';
-
+import { Table } from "react-bootstrap";
+import './index.css';
 
 export default function TabelaSeparacao() {
   return (
-    <div className="App">
-      <label htmlFor="Protocolo">Protocolo</label>
+    <div className="app-container">
+      <div className="input-container">
+        <div>
+           <label className="label">Protocolo</label>
+           <input type="text"  className="input"/>
+        </div>
+  
+        <div className="containerBotao">
+          <button className="btn btn-separacao">SEPARAÇÃO</button>
+          <button className="btn btn-limpeza">LIMPEZA</button>
+        </div>
+      </div>
+
+      <div className="table-container">
+        <Table className="custom-table">
+          <thead>
+            <tr>
+              <th >Protocolo</th>
+              <th>Start Bit</th>
+              <th>Tamanho do Pacote</th>
+              <th>Numero do Protocolo</th>
+              <th>Informação Contida</th>
+              <th>Informação do Numero Sequencia</th>
+              <th>Error Check</th>
+              <th>Stop Bit</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td >teste</td>
+              <td>teste</td>
+              <td>teste</td>
+              <td>teste</td>
+              <td>teste</td>
+              <td>teste</td>
+              <td>teste</td>
+              <td>teste</td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
     </div>
   );
-}  
-
-
+}
